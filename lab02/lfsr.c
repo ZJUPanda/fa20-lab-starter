36ce703b63ba5edd6cf8f474541aed1a11a2d354
P310S <p310s@P310SdeiMac.lan>
2021-11-08 22:45:58 +0800

    lab0-2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,14 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+
+    unsigned b0, b2, b3, b5;
+    b0 = (*reg) & 1u;
+    b2 = (*reg>>2) & 1u;
+    b3 = (*reg>>3) & 1u;
+    b5 = (*reg>>5) & 1u;
+    unsigned shift_in = b0^b2^b3^b5;
+    *reg >>= 1;
+    *reg |= shift_in<<15;
 }
 
